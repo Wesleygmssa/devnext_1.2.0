6c7f1dd13ee1232abaee5218bfc3719073bffc97
Wesley Guerra <guerra_ssa@hotmail.com>
2021-08-12 18:57:33 -0300

    Configurando autenticação co github

--- compasso/src/pages/api/users/index.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default (request: NextApiRequest, response: NextApiResponse) => {
-  const users = [{ id: 1, name: "Wesley" }];
-
-  return response.json(users);
-};
