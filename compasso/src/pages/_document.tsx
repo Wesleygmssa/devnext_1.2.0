8ba5b590e0aac01c9462129eea1f08ca9ceb38c6
wesley guerra <wesleyguerra9@gmail.com>
2021-08-12 16:03:35 -0300

    finalizando header

--- compasso/src/pages/_document.tsx
+++ compasso/src/pages/_document.tsx
@@ -11,6 +11,9 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
             rel="stylesheet"
           />
+
+          {/* Favicon */}
+          {/* <link rel="shortcut icon" href="" type="image/x-icon" /> */}
         </Head>
         <body>
           <Main />
