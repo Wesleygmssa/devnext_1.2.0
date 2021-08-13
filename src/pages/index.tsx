eb8aefd0031043b0618bf189316085f9acaa00fe
Wesley Guerra <guerra_ssa@hotmail.com>
2021-08-13 18:28:06 -0300

    finalizado página home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,24 @@
 /* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
-import styles from "./home.modules.scss";
+import styles from "./home.module.scss";
 export default function Home() {
   return (
     <>
       <Head>
         <title>Document</title>
       </Head>
-      <main>
-        <section>
+      <main className={styles.contentContainer}>
+        <section className={styles.here}>
           <span>Hey, welcome</span>
           <h1>
             Buscar por <span>Devs</span>{" "}
+            <p>Veja seus repositórios e o mais acessados.</p>
           </h1>
-          <p>Veja seus repositórios e o mais acessados.</p>
         </section>
         <img
           src="https://d335luupugsy2.cloudfront.net/images%2Flanding_page%2F1314146%2Ftopo-email.jpg"
           alt="Image"
+          width="500"
         />
       </main>
     </>
